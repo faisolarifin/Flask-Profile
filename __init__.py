42c3c60854c9c4a599ae06af896933a208afc87a
Ach. Faisol S. Arifin <54441307+achfaisol@users.noreply.github.com>
2020-07-26 20:09:47 +0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=1)
+
