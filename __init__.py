962448c416b1d6d42ab1bf1a2750c8314126ac18
Ach. Faisol S. Arifin <54441307+achfaisol@users.noreply.github.com>
2020-07-27 13:37:48 +0700

    menambahkan skill dan contack

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/curriculum')
+def cv():
+    return "belum ada"
+
 if __name__ == "__main__":
     app.run(debug=1)
 
